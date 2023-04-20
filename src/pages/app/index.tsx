a657c364d684752a6df5ca519676b49c622e764d
davidlewallen <lewallen.david@gmail.com>
2023-04-19 23:46:09 -0500

    feat(client): allows reminders to be completed

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -4,10 +4,13 @@ import { api } from "~/utils/api";
 const App = () => {
   const utils = api.useContext();
   const [value, setValue] = useState("");
+  const { data: remindersData } = api.reminders.getAll.useQuery();
   const { mutate: addReminder } = api.reminders.add.useMutation({
     onSuccess: () => utils.reminders.getAll.invalidate(),
   });
-  const { data: remindersData } = api.reminders.getAll.useQuery();
+  const { mutate: completeReminder } = api.reminders.complete.useMutation({
+    onSuccess: () => utils.reminders.getAll.invalidate(),
+  });
 
   return (
     <main>
@@ -26,9 +29,29 @@ const App = () => {
         />
         <button type="submit">Add</button>
       </form>
-      {remindersData?.reminders.map((reminder) => (
-        <div key={reminder.id}>{reminder.text}</div>
-      ))}
+      {remindersData?.reminders
+        .sort((a, b) =>
+          (a?.completedAt ?? 0) > (b?.completedAt ?? 0) ? -1 : 1
+        )
+        .sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1))
+        .sort((a, b) => (a.completed ? 1 : b.completed ? -1 : 0))
+
+        .map((reminder) => (
+          // TODO: Update from div to actual button element once implementing a ui libray
+          <div
+            key={reminder.id}
+            className={
+              reminder.completed
+                ? "cursor-default line-through"
+                : "cursor-pointer"
+            }
+            {...(reminder.completed
+              ? {}
+              : { onClick: () => completeReminder(reminder.id) })}
+          >
+            {reminder.text}
+          </div>
+        ))}
     </main>
   );
 };
