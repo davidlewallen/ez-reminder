9a5362d273e8232c7609338f3d60c35e965386cf
davidlewallen <lewallen.david@gmail.com>
2023-05-06 23:26:22 -0500

    refactor: moves hook logic into own file

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -6,90 +6,50 @@ import {
   CardBody,
   IconButton,
   Text,
-  type ToastId,
-  useToast,
 } from "@chakra-ui/react";
-import { useCallback, useRef } from "react";
 import { type RouterOutputs } from "~/utils/api";
 
 type Reminders = RouterOutputs["reminders"]["getAll"];
 type Reminder = Reminders[number];
 
-export const useNotification = () => {
-  const toast = useToast();
-  const toastIdRef = useRef<ToastId[]>([]);
+interface Props {
+  reminder: Reminder;
+  handleComplete: (reminderId: Reminder["id"]) => void;
+  handleDismiss: (reminderId: Reminder["id"]) => void;
+  handleSnooze: (reminderId: Reminder["id"]) => void;
+}
 
-  const handleSnooze = useCallback(
-    (reminderId: ToastId) => {
-      // Do something with snooze
-      if (toastIdRef.current.includes(reminderId)) toast.close(reminderId);
-    },
-    [toast]
-  );
-
-  const handleDismiss = useCallback(
-    (reminderId: ToastId) => {
-      // Do something with dismiss
-      if (toastIdRef.current.includes(reminderId)) toast.close(reminderId);
-    },
-    [toast]
-  );
-
-  const handleComplete = useCallback(
-    (reminderId: ToastId) => {
-      // Do something with complete
-      if (toastIdRef.current.includes(reminderId)) toast.close(reminderId);
-    },
-    [toast]
-  );
-
-  const generateNotification = (reminder: Reminder) => {
-    if (!toast.isActive(reminder.id)) {
-      toastIdRef.current.push(
-        toast({
-          duration: null,
-          id: reminder.id,
-          position: "top-right",
-          render: () => (
-            <Card>
-              <CardBody display="flex" flexDirection="column">
-                <Box marginBottom="2">
-                  <Text fontWeight="medium">{reminder.text}</Text>
-                </Box>
-                <ButtonGroup>
-                  <IconButton
-                    icon={<MoonIcon />}
-                    aria-label="snooze reminder"
-                    colorScheme="blue"
-                    onClick={() => handleSnooze(reminder.id)}
-                  />
-                  <IconButton
-                    icon={<CloseIcon />}
-                    aria-label="dismiss reminder"
-                    colorScheme="red"
-                    onClick={() => handleDismiss(reminder.id)}
-                  />
-                  <IconButton
-                    icon={<CheckIcon />}
-                    aria-label="complete reminder"
-                    colorScheme="green"
-                    onClick={() => handleComplete(reminder.id)}
-                  />
-                </ButtonGroup>
-              </CardBody>
-            </Card>
-          ),
-        })
-      );
-    }
-  };
-
-  return {
-    createNotification: useCallback(generateNotification, [
-      handleComplete,
-      handleDismiss,
-      handleSnooze,
-      toast,
-    ]),
-  };
-};
+export const Notification = ({
+  reminder,
+  handleComplete,
+  handleDismiss,
+  handleSnooze,
+}: Props) => (
+  <Card>
+    <CardBody display="flex" flexDirection="column">
+      <Box marginBottom="2">
+        <Text fontWeight="medium">{reminder.text}</Text>
+      </Box>
+      <ButtonGroup>
+        <IconButton
+          icon={<MoonIcon />}
+          aria-label="snooze reminder"
+          colorScheme="blue"
+          onClick={() => handleSnooze(reminder.id)}
+        />
+        <IconButton
+          icon={<CloseIcon />}
+          aria-label="dismiss reminder"
+          colorScheme="red"
+          onClick={() => handleDismiss(reminder.id)}
+        />
+        <IconButton
+          icon={<CheckIcon />}
+          aria-label="complete reminder"
+          colorScheme="green"
+          onClick={() => handleComplete(reminder.id)}
+        />
+      </ButtonGroup>
+    </CardBody>
+  </Card>
+);
