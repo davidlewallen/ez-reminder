a5bb47d6ff78feb7b1500aaf1b4c7ed1cf8b6854
davidlewallen <lewallen.david@gmail.com>
2023-05-08 22:27:08 -0500

    feat: render reminder notifications

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,16 @@
 import { Container } from "@chakra-ui/react";
 import { type ReactNode } from "react";
 import { Navbar } from "./Navbar";
+import { useNotification } from "~/hooks/use-notification";
 
-export const Layout = ({ children }: { children: ReactNode }) => (
-  <Navbar>
-    <Container height="100%" overflow="hidden">
-      <main className="h-full overflow-hidden">{children}</main>
-    </Container>
-  </Navbar>
-);
+export const Layout = ({ children }: { children: ReactNode }) => {
+  useNotification();
+
+  return (
+    <Navbar>
+      <Container height="100%" overflow="hidden">
+        <main className="h-full overflow-hidden">{children}</main>
+      </Container>
+    </Navbar>
+  );
+};
