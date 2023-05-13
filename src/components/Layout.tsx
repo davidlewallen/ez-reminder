2936f8d9d4e602f836438d7c0dc700fe57a81d89
davidlewallen <lewallen.david@gmail.com>
2023-05-13 10:37:50 -0500

    feat: allow Layout to hide navbar with prop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,20 @@ import { type ReactNode } from "react";
 import { Navbar } from "./Navbar";
 import { useNotification } from "~/hooks/use-notification";
 
-export const Layout = ({ children }: { children: ReactNode }) => {
-  useNotification();
+export const Layout = ({
+  children,
+  hideNavbar = false,
+}: {
+  children: ReactNode;
+  hideNavbar?: boolean;
+}) => {
+  useNotification(hideNavbar);
 
-  return (
+  return hideNavbar ? (
+    <Container height="100%" overflow="hidden">
+      <main className="h-full overflow-hidden">{children}</main>
+    </Container>
+  ) : (
     <Navbar>
       <Container height="100%" overflow="hidden">
         <main className="h-full overflow-hidden">{children}</main>
